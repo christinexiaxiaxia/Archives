8c3b4aecba3ed7208cd203ffc97ddc312d641f15
Christine <christine-xia@hotmail.com>
2019-12-02 06:59:20 -0500

    The Database!!!!

--- js/main.js
+++ js/main.js
@@ -22,11 +22,13 @@ $('.main-title').hover(function(){
 var windowAttr = 'height=620,width=950,left=425,top=167,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes';
 
 
+
 // RECORDS
 
 $('.records').mousedown(function(){
 	window.open('records/index.html','theRecordsInventory', windowAttr)
 })
+
 
 
 // SECTION 01
@@ -114,15 +116,15 @@ $('.item.02-b-01').mousedown(function(){
 ///////// FOLDER C
 
 $('.item.02-c-03').mousedown(function(){
-	window.open('items/02/C/01/index.html','itemWindow', windowAttr)
+	window.open('items/02/C/03/index.html','itemWindow', windowAttr)
 })
 
 $('.item.02-c-04').mousedown(function(){
-	window.open('items/02/C/01/index.html','itemWindow', windowAttr)
+	window.open('items/02/C/04/index.html','itemWindow', windowAttr)
 })
 
 $('.item.02-c-05').mousedown(function(){
-	window.open('items/02/C/01/index.html','itemWindow', windowAttr)
+	window.open('items/02/C/05/index.html','itemWindow', windowAttr)
 })
 
 ///////// FOLDER D
@@ -169,6 +171,10 @@ $('.item.02-e-03').mousedown(function(){
 
 $('.item.03-a-01').mousedown(function(){
 	window.open('items/03/A/01/index.html','itemWindow', windowAttr)
+})
+
+$('.item.03-a-02').mousedown(function(){
+	window.open('items/03/A/02/index.html','itemWindow', windowAttr)
 })
 
 $('.item.03-a-03').mousedown(function(){
@@ -256,5 +262,60 @@ $('.item.03-d-02').mousedown(function(){
 $('.item.03-d-04').mousedown(function(){
 	window.open('items/03/D/04/index.html','itemWindow', windowAttr)
 })
+
+
+
+// ADDING HUMAN ARTIFACTS IMAGES
+
+for (var i = 0; i < 801; i++) {
+	var img = $('<img src="artifacts' + (i + 1) + '.jpeg">')
+	$('#human-artifacts').append(img);
+}
+
+
+
+// ADDING JAFFE IMAGES
+
+for (var i = 0; i < 213; i++) {
+	var img = $('<img src="jaffe' + (i + 1) + '.jpeg">')
+	$('#jaffe').append(img);
+}
+
+
+
+// ADDING ESSENTIALLY ANONYMOUS IMAGES
+
+for (var i = 0; i < 81; i++) {
+	var img = $('<img src="anon' + (i + 1) + '.jpg">')
+	$('#anon').append(img);
+}
+
+
+
+// ADDING COLORIZED PORTRAIT IMAGES
+
+for (var i = 0; i < 9; i++) {
+	var img = $('<img src="colorized' + (i + 1) + '.jpg">')
+	$('#color').append(img);
+}
+
+
+
+// ADDING AMNH MUSEUM AND DISPLAY IMAGES
+
+for (var i = 0; i < 133; i++) {
+	var img = $('<img src="museum' + (i + 1) + '.jpg">')
+	$('#museum').append(img);
+}
+
+
+
+// ADDING RUINS IMAGES
+
+for (var i = 0; i < 27; i++) {
+	var img = $('<img src="ruins' + (i + 1) + '.jpg">')
+	$('#ruins').append(img);
+}
+
 
 
